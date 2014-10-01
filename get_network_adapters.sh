7b66800e77a1bf6d682847afe1f81ec4d035638c
Tobias Schaaf (meveric) <schlurf82@googlemail.com>
2014-10-01 14:56:26 +0200

    * added script to choose network adapter

---
+++ get_network_adapters.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+num_devices=`ip addr show | grep eth.: | wc -l`
+if [ $num_devices -gt 1 ]; then
+	i=0
+	menu=""
+	while [ $i -lt $num_devices ]
+	do
+		if [ $menu == "" ]; then
+			menu="$i eth$i"
+		else
+			menu="$menu\ $i eth$i"
+		fi
+		i=$(($i+1))
+	done
+	CC=$(whiptail --backtitle "$TITLE" --menu "Select ethernet device" 0 0 1 --cancel-button "Exit" --ok-button "Select" \
+	$menu \
+	3>&1 1>&2 2>&3)
+	result=$?
+	if [ $result -eq 0 ]; then
+		echo "eth$CC"
+	fi
+else
+	echo "eth0"
+fi
