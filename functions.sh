731c44c5ff66044264c91300c8bbe095d5e5ccf1
Tobias Schaaf (meveric) <schlurf82@googlemail.com>
2014-11-18 10:40:15 +0100

    fixing an issue with the detection of the latest updates

--- functions.sh
+++ functions.sh
@@ -6,10 +6,14 @@ msgbox() {
 }
 get_updates() {
 	# only check for updates once a day not every single time
-	LAST_UPDATE=`stat -c %Y /var/cache/apt/pkgcache.bin`
-	DATE=`date +%s`
-	UPDATE_AGE=$(($DATE-$LAST_UPDATE))
-	if [ $UPDATE_AGE -gt $((60*60*24)) ]; then
+	if [ -f /var/cache/apt/pkgcache.bin ]; then
+		LAST_UPDATE=`stat -c %Y /var/cache/apt/pkgcache.bin`
+		DATE=`date +%s`
+		UPDATE_AGE=$(($DATE-$LAST_UPDATE))
+		if [ $UPDATE_AGE -gt $((60*60*24)) ]; then
+			apt-get update
+		fi
+	else
 		apt-get update
 	fi
 }
